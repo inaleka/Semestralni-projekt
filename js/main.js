c8413f65c15dd146c9d5cceffb7cacc87d212418
Inal Ekashaev <inal.ekashaev@tul.cz>
2024-05-22 00:44:52 +0200

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 const APIKEY = '572dcf998f37c9a7c97cbfeec2eaa00a'
 
-console.log('Hello!');
+const form = document.querySelector('#form');
+const input = document.querySelector('.form__input')
+
+
+form.onsubmit = submitHandler;
+
+function submitHandler(e){
+    e.preventDefault();
+
+
+    if(!input.value){        
+        console.log('Enter city name')
+        return
+    }
+    
+    
+    console.log(input.value)
+    
+}
