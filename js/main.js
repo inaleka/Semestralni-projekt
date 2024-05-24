62d027a3b73d2679c6b1dc6234b77ae83479f380
Inal Ekashaev <inal.ekashaev@tul.cz>
2024-05-24 16:50:08 +0200

    oprava main.js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,27 @@
 const API_KEY = '572dcf998f37c9a7c97cbfeec2eaa00a';
-
 const form = document.querySelector('#form');
 const input = document.querySelector('.form__input');
+let isLoggedIn = false; 
+form.onsubmit = submitHandler;
 
-let isLoggedIn = false;
-
-if (form) {
-  form.onsubmit = submitHandler;
-}
-
-export async function submitHandler(e) {
+async function submitHandler(e) {
   e.preventDefault();
 
   if (!input.value.trim()) {
     console.log('Enter city name');
     return;
   }
-
   const cityName = input.value.trim();
   input.value = '';
-
   const cityInfo = await getGeo(cityName);
-
   if (!cityInfo.length) return;
-
   const weatherInfo = await getWeather(
     cityInfo[0]['lat'],
     cityInfo[0]['lon']
   );
-
   console.log(weatherInfo.name);
   console.log(weatherInfo.weather[0]['main']);
-
   input.value = '';
-
   const weatherData = {
     name: weatherInfo.name,
     temp: weatherInfo.main.temp,
@@ -41,40 +29,32 @@ export async function submitHandler(e) {
     speed: weatherInfo.wind.speed,
     main: weatherInfo.weather[0]['main']
   };
-
   renderWeatherData(weatherData);
 }
-
-export async function getGeo(name) {
-  const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${API_KEY}`;
+async function getGeo(name) {
+  const geoUrl = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${API_KEY}`;
   const response = await fetch(geoUrl);
   const data = await response.json();
   return data;
 }
-
-export async function getWeather(lat, lon) {
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+async function getWeather(lat, lon) {
+  const weatherUrl = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
   const response = await fetch(weatherUrl);
   const data = await response.json();
   return data;
 }
-
-export function renderWeatherData(data) {
+function renderWeatherData(data) {
   document.querySelector('.weather__info').classList.remove('none');
   document.querySelector('.weather__details').classList.remove('none');
-
   const temp = document.querySelector('.weather__temp');
   const city = document.querySelector('.weather__city');
   const humidity = document.querySelector('#humidity');
   const speed = document.querySelector('#speed');
   const img = document.querySelector('.weather__img');
-
   temp.innerText = Math.round(data.temp) + '°c';
   city.innerText = data.name;
-
   humidity.innerText = data.humidity + '%';
   speed.innerText = data.speed + 'km/h';
-
   const fileNames = {
     Clouds: 'clouds',
     Clear: 'clear',
@@ -83,7 +63,6 @@ export function renderWeatherData(data) {
     Mist: 'mist',
     Drizzle: 'drizzle'
   };
-
   if (fileNames[data.main]) {
     img.src = `./img/weather/${fileNames[data.main]}.png`;
   }
